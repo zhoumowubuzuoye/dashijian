6d8282fe72f2a0ffa227997e65a52f439acc101e
周末勿补作业 <1078637049@qq.com>
2020-08-27 21:23:17 +0800

    login基本完成

--- 大事件静态资源/BigNews/admin/js/login.js
+++ 大事件静态资源/BigNews/admin/js/login.js
@@ -7,8 +7,13 @@ $(function(){
       url:"http://localhost:8080/api/v1/admin/user/login",
       data:$(this).serialize(),
       success:function(info){
+        $("#myModal").modal('show')
+        $(".modal-body p").text(info.msg)
         if(info.code ===200){
-          console.log(123);
+          window.localStorage.setItem("token", backData.token);
+          $('#myModal').on('hidden.bs.modal', function (e) {
+            location.herf ="./index.html"
+          })
         }
       }
 
