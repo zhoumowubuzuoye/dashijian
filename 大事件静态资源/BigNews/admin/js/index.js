75e6f29fe3497a53a09d77ed4625a8cf6ceaee94
周末勿补作业 <1078637049@qq.com>
2020-08-28 15:44:12 +0800

    添加了退出和登录

--- 大事件静态资源/BigNews/admin/js/index.js
+++ 大事件静态资源/BigNews/admin/js/index.js
@@ -1,9 +1,20 @@
 $(function(){
+  var $usepic = $(".user_info img")
+  var $header = $(".header_bar img")
+  var $exit = $(".user_center_link .exit")
   $.ajax({
     type:"get",
     url:"http://localhost:8080/api/v1/admin/user/info",
     success:function(info){
-      console.log(123);
+      console.log(info);
+      if(info.code===200){
+        $usepic.attr("src","http://localhost:8080/icon.jpg")
+        $header.attr("src","http://localhost:8080/icon.jpg")
+      }
     }
+  })
+  $exit.on("click",function(){
+    localStorage.removeItem("token")
+    location.href = "./login.html"
   })
 })
