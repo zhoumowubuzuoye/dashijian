5e0875b3f381272802f32e94eaac7b1ec7a4bd95
周末勿补作业 <1078637049@qq.com>
2020-09-04 09:01:15 +0800

    ?

--- 大事件静态资源/BigNews/admin/js/article_list.js
+++ 大事件静态资源/BigNews/admin/js/article_list.js
@@ -1,106 +1,92 @@
 $(function () {
-
   $.ajax({
     url: "http://localhost:8080/api/v1/admin/category/list",
     success: function (info) {
-      // console.log(info);
-      if (info.code == 200) {
-        var str = template("alloption", info)
-        $("#selCategory").html(str)
-      }
+      var htmlStr = template("alloption", info)
+      $("#selCategory").html(htmlStr)
     }
   })
   $.ajax({
-    url:"http://localhost:8080/api/v1/admin/article/query",
-    success:function(info){
-      console.log(info);
-      var str = template("allclass",info.data)
-      $("tbody").html(str)
-      if(info.data.totalCount==0){
-        // console.log(123);
-        $("#pagination-demo").hide().next().show()
-      }else{
-        $("#pagination-demo").show().next().hide()
-        // console.log(456);
+    url: "http://localhost:8080/api/v1/admin/article/query",
+    success: function (info) {
+      if (info.code == 200) {
+        if (info.totalCount == 0) {
+          $("#pagination-demo").hide().next().show()
+        } else {
+          $("#pagination-demo").show().next().hide()
+          var htmlStr = template("allclass", info.data)
+          $("tbody").html(htmlStr)
+         twbs(info)
+        }
       }
-      twbs(info)
+
     }
   })
-
- var currentPage=1
-  function twbs(res){
-    $("#pagination-demo").twbsPagination({
-      totalPages: res.data.totalPage, 
-      visiblePages: 6, 
-      first: '第一页',
-      last: '最后一页',
-      prev: '上一页',
-      next: '下一页',
+  function twbs(info){
+    $('#pagination-demo').twbsPagination({
+      totalPages: info.data.totalPage,
+      visiblePages: 6,
+      // startPage:1,
+      first:"第一页",
+      last:"最后一页",
+      prev:"上一页",
+      next:"下一页",
       initiateStartPageClick:false,
-      onPageClick:function(e,page){
-        //  $('#page-content').text('Page' + page)
-        // page表示当前被单击的页码值
+      onPageClick: function (event, page) {
        
-        currentPage = page,
+        thispage=page
+        // console.log(page);
         $.ajax({
-          type: 'get',
           url: "http://localhost:8080/api/v1/admin/article/query",
-          data: {
-            key: $('#myForm input[name=key]').val(), // 关键词
-            type: $('#selCategory').val(), // 文章分类 
-            state: $('#selStatus').val(), // 文章状态
-            page: page,
-            perpage: 6 // 页面中显示的数据条数
+          data:{
+            key: $("form [name=key]").val(),
+            type: $("form [name=type]").val(),
+            state: $("form [name=state]").val(), 
+            page:page,
+            perpage: 6
           },
-          success: function (res) {
-            // console.log(res)
-            // 2.2 将获取到的第1页文章数据渲染到页面中
-            if (res.code == 200) {
-              // 3.2 将服务器响应回来的数据渲染到页面上
-              var htmlStr = template('allclass', res.data)
-              $('tbody').html(htmlStr)
-            }
+          success: function (info) {
+            var htmlStr = template("allclass", info.data)
+            $("tbody").html(htmlStr)
           }
         })
-
       }
-
-    })
+    });
   }
-  $("#myForm").on("submit",function(e){
+  var thispage = 1
+  $("form").on("submit",function(e){
     e.preventDefault()
-    // console.log(123);
     $.ajax({
-      url:"http://localhost:8080/api/v1/admin/article/query",
+      url: "http://localhost:8080/api/v1/admin/article/query",
       data:{
-        key:$("form [name=key]").val(),
-        type:$("form [name=type]").val(),
-        state:$("form [name=state]").val(),
-        page:1,
-        perpage:6
+        key: $("form [name=key]").val(),
+        type: $("form [name=type]").val(),
+        state: $("form [name=state]").val(),
+        page: 1,
+        perpage: 6
       },
       success:function(info){
         console.log(info);
-        var str = template("allclass",info.data)
-        $("tbody").html(str)
-        if(info.data.totalCount==0){
-          // console.log(123);
-          $("#pagination-demo").hide().next().show()
-        }else{
-          $("#pagination-demo").show().next().hide()
-          // console.log(456);
-          $("#pagination-demo").twbsPagination("changeTotalPages",info.data.totalPage,1)
+        if (info.code == 200) {
+          if (info.data.totalCount == 0) {
+            console.log(123);
+            $("#pagination-demo").hide().next().show()
+            $("tbody").hide()
+          } else {
+            $("#pagination-demo").show().next().hide()
+            var htmlStr = template("allclass", info.data)
+            $("tbody").show().html(htmlStr)
+            $("#pagination-demo").twbsPagination("changeTotalPages",info.data.totalPage,1)
+          }
         }
       }
     })
   })
   $("#myModal").on("shown.bs.modal",function(e){
-    
+    console.log(e.relatedTarget);
     window.indexId=$(e.relatedTarget).data("id")
-    console.log(indexId);
   })
   $(".delSure").on("click",function(){
-
     $.ajax({
       type:"post",
       url:"http://localhost:8080/api/v1/admin/article/delete",
@@ -110,36 +96,224 @@ $(function () {
       success:function(info){
         console.log(info);
         $.ajax({
-          url:"http://localhost:8080/api/v1/admin/article/query",
+          url: "http://localhost:8080/api/v1/admin/article/query",
           data:{
-            key:$("form [name=key]").val(),
-            type:$("form [name=type]").val(),
-            state:$("form [name=state]").val(),
-            page:currentPage,
-            perpage:6
+            key: $("form [name=key]").val(),
+            type: $("form [name=type]").val(),
+            state: $("form [name=state]").val(),
+            page: thispage,
+            perpage: 6
           },
           success:function(info){
-            console.log(info);
-            var str = template("allclass",info.data)
-            $("tbody").html(str)
-            if(info.data.totalCount==0&&info.data.data.length == 0){
-              console.log(123);
-              $("#pagination-demo").hide().next().show()
-            }else{
-              $("#pagination-demo").show().next().hide()
-              console.log(456);
-              console.log(currentPage);
-              if (info.data.data.length == 0) {
-                currentPage -= 1
-                console.log(currentPage);
+            console.log(123);
+            if (info.code == 200) {
+              if (info.data.totalCount == 0&&info.data.data.length == 0) {
+                console.log(123);
+                $("#pagination-demo").hide().next().show()
+              } else {
+                console.log(456);
+                $("#pagination-demo").show().next().hide()
+                if(info.data.data.length == 0){
+                  thispage -=1
+                }
+                var htmlStr = template("allclass", info.data)
+                $("tbody").html(htmlStr)
+                $("#pagination-demo").twbsPagination("changeTotalPages",info.data.totalPage,thispage)
               }
-              $("#pagination-demo").twbsPagination("changeTotalPages",info.data.totalPage,currentPage)
             }
           }
         })
-        $("#myModal").modal("hide")
-      
       }
     })
   })
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(function () {
+
+//   $.ajax({
+//     url: "http://localhost:8080/api/v1/admin/category/list",
+//     success: function (info) {
+//       // console.log(info);
+//       if (info.code == 200) {
+//         var str = template("alloption", info)
+//         $("#selCategory").html(str)
+//       }
+//     }
+//   })
+//   $.ajax({
+//     url:"http://localhost:8080/api/v1/admin/article/query",
+//     success:function(info){
+//       console.log(info);
+//       var str = template("allclass",info.data)
+//       $("tbody").html(str)
+//       if(info.data.totalCount==0){
+//         // console.log(123);
+//         $("#pagination-demo").hide().next().show()
+//       }else{
+//         $("#pagination-demo").show().next().hide()
+//         // console.log(456);
+//       }
+//       twbs(info)
+//     }
+//   })
+
+//  var currentPage=1
+//   function twbs(res){
+//     $("#pagination-demo").twbsPagination({
+//       totalPages: res.data.totalPage, 
+//       visiblePages: 6, 
+//       first: '第一页',
+//       last: '最后一页',
+//       prev: '上一页',
+//       next: '下一页',
+//       initiateStartPageClick:false,
+//       onPageClick:function(e,page){
+//         //  $('#page-content').text('Page' + page)
+//         // page表示当前被单击的页码值
+
+//         currentPage = page,
+//         $.ajax({
+//           type: 'get',
+//           url: "http://localhost:8080/api/v1/admin/article/query",
+//           data: {
+//             key: $('#myForm input[name=key]').val(), // 关键词
+//             type: $('#selCategory').val(), // 文章分类 
+//             state: $('#selStatus').val(), // 文章状态
+//             page: page,
+//             perpage: 6 // 页面中显示的数据条数
+//           },
+//           success: function (res) {
+//             // console.log(res)
+//             // 2.2 将获取到的第1页文章数据渲染到页面中
+//             if (res.code == 200) {
+//               // 3.2 将服务器响应回来的数据渲染到页面上
+//               var htmlStr = template('allclass', res.data)
+//               $('tbody').html(htmlStr)
+//             }
+//           }
+//         })
+
+//       }
+
+//     })
+//   }
+//   $("#myForm").on("submit",function(e){
+//     e.preventDefault()
+//     // console.log(123);
+//     $.ajax({
+//       url:"http://localhost:8080/api/v1/admin/article/query",
+//       data:{
+//         key:$("form [name=key]").val(),
+//         type:$("form [name=type]").val(),
+//         state:$("form [name=state]").val(),
+//         page:1,
+//         perpage:6
+//       },
+//       success:function(info){
+//         console.log(info);
+//         var str = template("allclass",info.data)
+//         $("tbody").html(str)
+//         if(info.data.totalCount==0){
+//           // console.log(123);
+//           $("#pagination-demo").hide().next().show()
+//         }else{
+//           $("#pagination-demo").show().next().hide()
+//           // console.log(456);
+//           $("#pagination-demo").twbsPagination("changeTotalPages",info.data.totalPage,1)
+//         }
+//       }
+//     })
+//   })
+//   $("#myModal").on("shown.bs.modal",function(e){
+
+//     window.indexId=$(e.relatedTarget).data("id")
+//     console.log(indexId);
+//   })
+//   $(".delSure").on("click",function(){
+
+//     $.ajax({
+//       type:"post",
+//       url:"http://localhost:8080/api/v1/admin/article/delete",
+//       data:{
+//         id:indexId
+//       },
+//       success:function(info){
+//         console.log(info);
+//         $.ajax({
+//           url:"http://localhost:8080/api/v1/admin/article/query",
+//           data:{
+//             key:$("form [name=key]").val(),
+//             type:$("form [name=type]").val(),
+//             state:$("form [name=state]").val(),
+//             page:currentPage,
+//             perpage:6
+//           },
+//           success:function(info){
+//             console.log(info);
+//             var str = template("allclass",info.data)
+//             $("tbody").html(str)
+//             if(info.data.totalCount==0&&info.data.data.length == 0){
+//               console.log(123);
+//               $("#pagination-demo").hide().next().show()
+//             }else{
+//               $("#pagination-demo").show().next().hide()
+//               console.log(456);
+//               console.log(currentPage);
+//               if (info.data.data.length == 0) {
+//                 currentPage -= 1
+//                 console.log(currentPage);
+//               }
+//               $("#pagination-demo").twbsPagination("changeTotalPages",info.data.totalPage,currentPage)
+//             }
+//           }
+//         })
+//         $("#myModal").modal("hide")
+
+//       }
+//     })
+//   })
+// })
---
+++ 大事件静态资源/BigNews/admin/js/article_release.js
@@ -0,0 +1,51 @@
+$(function () {
+  $.ajax({
+    url: "http://localhost:8080/api/v1/admin/category/list",
+    success: function (info) {
+      console.log(123);
+      var htmlstr = template("opt", info)
+      console.log(htmlstr);
+      $("[name=categoryId").html(htmlstr)
+    }
+
+  })
+  $("[name=cover]").on("change", function () {
+
+    var file = this.files[0]
+    var url = URL.createObjectURL(file)
+    console.log($(this).prev());
+    $(this).prev().attr("src", url)
+  })
+  jeDate("#testico", {
+    onClose: false,
+    theme: { bgcolor: "#00A680", pnColor: "#00DDAA" },
+    format: "YYYY-MM-DD "
+  });
+  var E = window.wangEditor
+  var editor = new E('#editor')
+  editor.create();
+  $("#myform").on("click", ".btn", function (e) {
+    e.preventDefault()
+    // var data = new FormData($("#form")[0])
+    var data = new FormData($('#myform')[0])
+    console.log(data);
+    data.append("content", editor.txt.html())
+    if ($(this).hasClass('btn-release')) {
+      data.append("state", "已发布")
+    } else {
+      data.append("state", "草稿")
+    }
+    console.log(data);
+    $.ajax({
+      type: "post",
+      url: "http://localhost:8080/api/v1/admin/article/publish",
+      contentType: false,
+      processData: false,
+      data: data,
+      success: function (info) {
+        console.log(info);
+      }
+    })
+  })
+
+})
